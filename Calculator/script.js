b3cb0428e129520dff966b81d2cca7882d2ec2d1
shivamgupta002 <99163318+shivamgupta002@users.noreply.github.com>
2022-08-15 02:28:43 -0700

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,27 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    }
+   else if (e.target.innerHTML == "C") {
+      string ="";
+      document.querySelector("input").value = string;
+    }
+   else if (e.target.innerHTML == "DEL") {
+    for(let i=string.length;i>=0;i--){
+        let d=string.slice(0,-1);
+        document.querySelector("input").value = string.pop;
+        break;
+    }
+    //    document.querySelector("input").value = string.slice(0,-1);
+    }
+     else {
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+    
+  });
+});
